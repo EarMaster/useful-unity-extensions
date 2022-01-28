69b4458b3eb97b10b321728b4b5fe2b4f2909cec
Nico Wiedemann <nico.wiedemann@visionsbox.de>
2022-01-28 17:07:34 +0100

    Tween Reference

--- Runtime/CanvasGroupExtensions.cs
+++ Runtime/CanvasGroupExtensions.cs
@@ -1,7 +1,9 @@
 namespace VISIONSBOX.Extensions {
 	using UnityEngine;
 	using UnityEngine.UI;
+	#if SURGE_TWEEN_AVAILABLE
 	using Pixelplacement;
+	#endif
 
 	public static class CanvasGroupExtensions {
 		public static float ChangeDuration = 0.25f;
@@ -25,15 +27,16 @@ namespace VISIONSBOX.Extensions {
 		}
 		/// <summary>Makes the CanvasGroup visible.</summary>
 		public static void Show(this CanvasGroup CanvasGroup, float? Duration = null, float? Delay = null, bool SetInvisibleBefore = false) {
-			if (Application.isEditor && !Application.isPlaying)
-				CanvasGroup.ShowImmediate();
-			else {
+			#if SURGE_TWEEN_AVAILABLE
+			if (!(Application.isEditor && !Application.isPlaying)) {
 				if (SetInvisibleBefore)
 					CanvasGroup.HideImmediate(false);
 				CanvasGroup.interactable = true;
 				Tween.CanvasGroupAlpha(CanvasGroup, 1f, Duration.HasValue ? Duration.Value : ChangeDuration, Delay.HasValue ? Delay.Value : 0f, null, Tween.LoopType.None,
 				                       () => CanvasGroup.gameObject.SetActive(true), null);
-			}
+			} else
+			#endif
+			CanvasGroup.ShowImmediate();
 		}
 		/// <summary>Makes the CanvasGroup visible immediatly without transition.</summary>
 		public static void ShowImmediate(this CanvasGroup CanvasGroup) {
@@ -44,15 +47,16 @@ namespace VISIONSBOX.Extensions {
 		/// <summary>Hides the CanvasGroup.</summary>
 		[ContextMenu("Hide")]
 		public static void Hide(this CanvasGroup CanvasGroup, float? Duration = null, float? Delay = null, bool SetVisibleBefore = false, bool SetInactive = true) {
-			if (Application.isEditor && !Application.isPlaying)
-				CanvasGroup.HideImmediate(SetInactive);
-			else {
+			#if SURGE_TWEEN_AVAILABLE
+			if (!(Application.isEditor && !Application.isPlaying)) {
 				if (SetVisibleBefore)
 					CanvasGroup.ShowImmediate();
 				CanvasGroup.interactable = false;
 				Tween.CanvasGroupAlpha(CanvasGroup, 0f, Duration.HasValue ? Duration.Value : ChangeDuration, Delay.HasValue ? Delay.Value : 0f, null, Tween.LoopType.None,
 				                       null, () => { if (SetInactive) CanvasGroup.gameObject.SetActive(false); });
-			}
+			} else
+			#endif
+			CanvasGroup.HideImmediate(SetInactive);
 		}
 		/// <summary>Hides the CanvasGroup, but keeps the GameObject active.</summary>
 		public static void HideButKeepActive(this CanvasGroup CanvasGroup, float? Duration = null, float? Delay = null, bool SetVisibleBefore = false) {
@@ -60,15 +64,16 @@ namespace VISIONSBOX.Extensions {
 		}
 		/// <summary>Fades the CanvasGroup and makes it non-interactible.</summary>
 		public static void Fade(this CanvasGroup CanvasGroup, float Alpha, float? Duration) {
-			if (Application.isEditor && !Application.isPlaying)
-				CanvasGroup.HideImmediate();
-			else {
+			#if SURGE_TWEEN_AVAILABLE
+			if (!(Application.isEditor && !Application.isPlaying)) {
 				CanvasGroup.interactable = false;
 				if (Duration.HasValue)
 					Tween.CanvasGroupAlpha(CanvasGroup, Alpha, Duration.Value, 0f);
 				else
 					Tween.CanvasGroupAlpha(CanvasGroup, Alpha, ChangeDuration, 0f);
-			}
+			} else
+			#endif
+			CanvasGroup.alpha = Alpha;
 		}
 		/// <summary>Hides the CanvasGroup immediatly without transition.</summary>
 		public static void HideImmediate(this CanvasGroup CanvasGroup, bool SetInactive = true) {
